9e8e80126e82233c3a037655c2e6e9d7e906320e
Jakub Kaczmarek <kuba300698@gmail.com>
2022-10-31 18:09:11 +0100

    Multiclass logistic regression

---
+++ multiclass_logistic regression/MulticlassLogisticRegression.py
@@ -0,0 +1,46 @@
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+
+def softmax(z):
+    return np.exp(z) / np.sum(np.exp(z))
+
+
+class MulticlassLogisticRegression:
+    def __init__(self, iters=1000, learning_rate=0.001):
+        self.weights = None
+        self.bias = None
+        self.iters = iters
+        self.learning_rate = learning_rate
+        self.onehot_encoder = OneHotEncoder(sparse=False)
+
+
+    def fit(self, X, y):
+        y_onehot = self.onehot_encoder.fit_transform(y.reshape(-1, 1))
+        n_samples, n_features = X.shape
+        C = y_onehot.shape[1]
+
+        self.weights = np.zeros((n_features, C))
+
+        for i in range(self.iters):
+            Z = - X @ self.weights
+            p = softmax(Z)
+            dw = 1 / n_samples * X.T @ (y_onehot - p) + 2 * self.weights
+
+            self.weights -= self.learning_rate * dw
+
+            if (i % 100 == 0):
+                print(f'Loss at {i} iteration: {self.loss(X, y_onehot)}')
+
+
+    def loss(self, X, y):
+        Z = - np.dot(X, self.weights)
+        n_samples = X.shape[0]
+
+        return (1 / n_samples) * (np.trace(X @ self.weights @ y.T) + np.sum(np.log(np.sum(np.exp(Z), axis=1))))
+
+
+    def predict(self, X):
+        Z = - X @ self.weights
+        P = softmax(Z)
+
+        return np.argmax(P, axis=1)
---
+++ multiclass_logistic regression/train.py
@@ -0,0 +1,18 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from MulticlassLogisticRegression import MulticlassLogisticRegression
+from sklearn.metrics import accuracy_score
+
+dataset = datasets.load_digits()
+
+X, y = dataset.data, dataset.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = MulticlassLogisticRegression(iters=10000, learning_rate=0.001)
+
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print(accuracy_score(y_test, y_pred))
+
